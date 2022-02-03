2b76e224ca79d50f98535e5ea7df5d82e1780de2
Harel Ashwal <harel.ashwal@opsys-tech.com>
2022-02-03 22:38:50 +0200

    fix annoying windows bug

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,16 @@
 # fix windows registry stuff
+from flask_socketio import SocketIO, emit
+import base64
+import cv2
+import pathlib
+from flask import Flask, send_from_directory, redirect, jsonify
 import mimetypes
 mimetypes.add_type('application/javascript', '.js')
 mimetypes.add_type('text/css', '.css')
 
-from flask import Flask, send_from_directory, redirect, jsonify
-from flask_socketio import SocketIO,emit
 
-import pathlib
-import cv2
-import base64
-
-cap=cv2.VideoCapture(0)  ##when removing debug=True or using gevent or eventlet uncomment this line and comment the cap=cv2.VideoCapture(0) in gen(json)
+# when removing debug=True or using gevent or eventlet uncomment this line and comment the cap=cv2.VideoCapture(0) in gen(json)
+cap = cv2.VideoCapture(0)
 app = Flask(__name__)
 # app.debug=True
 # app.config['SECRET_KEY'] = '78581099#lkjh'
@@ -29,29 +29,44 @@ def static_proxy(path: str):
 def root():
     return send_from_directory("../frontend/dist/frontend", "index.html")
 
-@socketio.on('check')
+
+global stopit
+stopit = False
+
+
+@socketio.on('stop')
+def stopit():
+    global stopit
+    stopit = not stopit
+
+
+@socketio.on('start')
 def gen():
-	# cap=cv2.VideoCapture(0)	
-	while(cap.isOpened()):
-		ret,img=cap.read()
-		if ret:
-			# img = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
-			frame = cv2.imencode('.jpg', img)[1].tobytes()
-			frame= base64.encodebytes(frame).decode("utf-8")
-			emit("image",frame)
-			socketio.sleep(0)
-		else:
-			break
+    global stopit
+    stopit = False
+    # cap=cv2.VideoCapture(0)
+    while(cap.isOpened()):
+        ret, img = cap.read()
+        if ret:
+            # img = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
+            frame = cv2.imencode('.jpg', img)[1].tobytes()
+            frame = base64.encodebytes(frame).decode("utf-8")
+            emit("image", frame)
+            socketio.sleep(0)
+            if stopit:
+                break
+        else:
+            break
 
 
-def message(json, methods=['GET','POST']):
-	# print("Recieved message")
-	socketio.emit('image', json )
+def message(json, methods=['GET', 'POST']):
+    # print("Recieved message")
+    socketio.emit('image', json)
+
 
 def run_main():
     socketio.run(app)
 
+
 if __name__ == "__main__":
     socketio.run(app)
-
-
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 import { SocketService } from './services/socket.service';
 
 @Component({
@@ -12,38 +13,58 @@ export class AppComponent {
 
   constructor(
     private socketService: SocketService,
+    public sanitizer: DomSanitizer
   ) {
     this.init();
+
   }
   title = 'frontend';
-  lastCalledTime : any;
-  fps : number;    
-  
-  init() {
-    this.socketService.socket.emit("check");
+  lastCalledTime: any;
+  fps: number;
+  zoomImage: SafeStyle;
+
+  isRunning = false;
+
+  async Delay(milliseconds: number) {
+    const result = new Promise<void>(resolve => {
+      setTimeout(resolve, milliseconds);
+    });
+    return result;
+  }
+
+  stop() {
+    this.socketService.socket.emit("stop");
+    this.isRunning = false;
+  }
+
+  start() {
+    this.socketService.socket.emit("start");
+    this.isRunning = true;
+  }
+
+  async init() {
+    this.stop();
+    await this.Delay(1000);
+
+    this.start();
+
     this.socketService.socket.on('image', (msg: any) => {
-      console.log("new image!!!");
       if (!this.lastCalledTime) {
         this.lastCalledTime = Date.now();
         this.fps = 0;
       }
-      else
-      {
-       let delta = (Date.now() - this.lastCalledTime) / 1000;
-       this.lastCalledTime = Date.now();
-       this.fps = 1 / delta;
+      else {
+        let delta = (Date.now() - this.lastCalledTime) / 1000;
+        this.lastCalledTime = Date.now();
+        this.fps = 1 / delta;
       }
-      
 
       const image_element = this.image1.nativeElement;
       image_element.src = "data:image/jpeg;base64," + msg;
-
+      this.zoomImage = this.sanitizer.bypassSecurityTrustUrl('url(' + image_element.src + ')');
       // let c = this.myCanvas.nativeElement;
       // let ctx = c.getContext("2d");
       // ctx.drawImage(image_element, 0, 0);
-
-
-
 
     });
   }
